2855e154a5e1e03b843cd5d825230e2892f62622
Raymond Z <raykyri@gmail.com>
2023-05-22 15:47:24 -0400

    set up map, player, terrain

--- packages/client/src/mud/createSystemCalls.ts
+++ packages/client/src/mud/createSystemCalls.ts
@@ -8,21 +8,62 @@ export type SystemCalls = ReturnType<typeof createSystemCalls>;
 
 export function createSystemCalls(
   { singletonEntity, playerEntity, worldSend, txReduced$ }: SetupNetworkResult,
-  components: ClientComponents
+  { Player, Position }: ClientComponents
 ) {
   const moveTo = async (x: number, y: number) => {
-    // TODO
-    return null as any;
+    if (!playerEntity) throw new Error("no player");
+    const positionId = uuid();
+    Position.addOverride(positionId, {
+      entity: playerEntity,
+      value: { x, y },
+    });
+
+    try {
+      const tx = worldSend("move", [x, y]);
+      await awaitStreamValue(txReduced$, (txHash) => txHash === tx.hash);
+    } finally {
+      Position.removeOverride(positionId);
+    }
   };
 
   const moveBy = async (deltaX: number, deltaY: number) => {
-    // TODO
-    return null as any;
+    if (!playerEntity) throw new Error("no player");
+
+    const playerPosition = getComponentValue(Position, playerEntity);
+    if (!playerPosition) {
+      console.warn("player not positioned");
+      return;
+    }
+
+    await moveTo(playerPosition.x + deltaX, playerPosition.y + deltaY);
   };
 
   const spawn = async (x: number, y: number) => {
-    // TODO
-    return null as any;
+    if (!playerEntity) throw new Error("no player");
+
+    // const positionId = uuid();
+    // Position.addOverride(positionId, {
+    //   entity: playerEntity,
+    //   value: { x, y },
+    // });
+    // const playerId = uuid();
+    // Player.addOverride(playerId, {
+    //   entity: playerEntity,
+    //   value: { value: true },
+    // });
+
+    const canSpawn = getComponentValue(Player, playerEntity)?.value !== true;
+    if (!canSpawn) {
+      throw new Error("Already spawned");
+    }
+
+    try {
+      const tx = await worldSend("spawn", [x, y]);
+      await awaitStreamValue(txReduced$, (txHash) => txHash === tx.hash);
+    } finally {
+      // Position.removeOverride(positionId);
+      // Player.removeOverride(playerId);
+    }
   };
 
   const throwBall = async () => {
--- packages/contracts/mud.config.ts
+++ packages/contracts/mud.config.ts
@@ -2,9 +2,26 @@ import { mudConfig } from "@latticexyz/world/register";
 
 export default mudConfig({
   enums: {
-    // TODO
+    TerrainType: ["None", "TallGrass", "Boulder"],
   },
   tables: {
-    // TODO
+    MapConfig: {
+      keySchema: {},
+      dataStruct: false,
+      schema: {
+        width: "uint32",
+        height: "uint32",
+        terrain: "bytes",
+      },
+    },
+    Movable: "bool",
+    Player: "bool",
+    Position: {
+      dataStruct: false,
+      schema: {
+        x: "uint32",
+        y: "uint32",
+      },
+    },
   },
 });
