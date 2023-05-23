a48a0e6b3ce07de0d53405a863f65ad5cd2e6600
Joel Gustafson <joelg@mit.edu>
2023-05-23 11:27:49 -0400

    bump protocol version

--- packages/client/src/libp2p/constants.ts
+++ packages/client/src/libp2p/constants.ts
@@ -6,6 +6,6 @@ export const PING_TIMEOUT = 10 * second;
 export const MIN_CONNECTIONS = 10;
 export const MAX_CONNECTIONS = 50;
 
-export const CHAT_TOPIC = "canvas:emojimon:chat:v0.1";
+export const CHAT_TOPIC = "canvas:emojimon:chat:v0.2";
 
 export const PEER_ID_KEY = "canvas:emojimon:peer-id";
